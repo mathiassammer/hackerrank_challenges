78c2e1adb72fe90aa82469131794df2609bd9c7a
Mathias Dantas <mathias.sammer.946@gmail.com>
2022-09-12 22:51:15 -0300

    Add solve_me_first challenge from algorithm category.

---
+++ src/main/kotlin/algorithms/solve_me_first.kt
@@ -0,0 +1,8 @@
+package algorithms
+
+fun main() {
+    solveMeFirst(5, 6) // return 11
+}
+
+// The solveMeFirst function is used to calculate the sum of two integers.
+fun solveMeFirst(a: Int, b: Int): Int = a + b
